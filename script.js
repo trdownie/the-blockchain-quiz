950df999312b8f8f991d14d9ae6a6a514da0aea2
trdownie <trdownie@gmail.com>
2021-04-19 13:09:02 +0000

    tidied welcome function layout and added invite as callback (not working)

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-
-    let i1 = 0;
-    let i2 = 0;
-document.addEventListener("DOMContentLoaded", function(){ welcomeIntro()
-
-function welcomeIntro(){
+    let i = 0;
+    let j = 0;
     let welcomeNew = 'Welcome to';
-    let title = '\n The Blockchain Quiz';
-    let invite = '<br><br>Shall we begin?';
+    let title = 'The Blockchain Quiz';
+    let invitation = 'Shall we begin?';
     let speed = 100;
     let lineBreak = document.createElement("br");
 
-    if (i1 < welcomeNew.length) {
-        document.getElementById("welcome-message").innerHTML += welcomeNew.charAt(i1);
-        i1++;
+function invite(){
+    if (j < invitation.length) {
+        document.getElementById("welcome-message").innerHTML += invitation.charAt(j);
+        j++;
+        setTimeout(invite, speed);
+    }
+}
+
+function welcomeIntro(invite){
+    if (i < welcomeNew.length) {
+        document.getElementById("welcome-message").innerHTML += welcomeNew.charAt(i);
+        i++;
         setTimeout(welcomeIntro, speed);
     }
     else {
         setTimeout(function() {
         document.getElementById("welcome-message").append(lineBreak);
         document.getElementById("welcome-message").append(title);
-    }, 1000);
+    }, 500);
+    invite()
     }
 }
-}
-)
+
+//this begins wecomeIntro() once DOM has loaded page content
+document.addEventListener("DOMContentLoaded", function(){ welcomeIntro(invite)
+})
