bc359b619d6fe78bed3cb6ce89e5d223ea043107
trdownie <trdownie@gmail.com>
2021-04-20 19:33:31 +0000

    added modal on 'begin' and tidied up some styling classes

--- index.js
+++ index.js
@@ -216,3 +216,23 @@ document.addEventListener("DOMContentLoaded",
     }, 6000),
 )
 
+//-------------------- 'BEGIN' MODAL 
+// code for modal taken from https://www.w3schools.com/howto/howto_css_modals.asp
+
+var modal = document.getElementById("modal");
+var btn = document.getElementById("begin-button");
+var span = document.getElementsByClassName("close-modal")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
