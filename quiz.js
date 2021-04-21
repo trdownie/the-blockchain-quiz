82e03b30bc2126d5b3c924145d8f7197eb9272ff
trdownie <trdownie@gmail.com>
2021-04-21 10:48:17 +0000

    added quiz framework to js and css with guiding notes

--- quiz.js
+++ quiz.js
@@ -4,3 +4,38 @@ function storeLevel(level) {
   document.cookie = userLevel + "=" + level + ";path=/";
 }
 
+//-------------------- QUIZ
+
+var questionOne = document.getElementById("question-one"); // targets form for question one
+var questionTwo = document.getElementById("question-two"); // targets form for question two
+var questionThree = document.getElementById("question-three"); // targets form for question three
+var questionFour = document.getElementById("question-four"); // etc
+var questionFive = document.getElementById("question-five");
+var questionSix = document.getElementById("question-six");
+var questionSeven = document.getElementById("question-seven");
+var questionEight = document.getElementById("question-eight");
+var questionNine = document.getElementById("question-nine");
+var questionTen = document.getElementById("question-ten");
+
+function runQuiz(){ // run master function on page load
+
+// default userLevel = 0
+// default userScore = 000
+
+    function displayQuestion() {
+// unhides q1 after 3 seconds & starts timer
+// on click of submit, assess whether right/wrong
+// if correct move userLevel up by 1, add timer count to userScore, run Q2
+// if incorrect display modal using level/score & stores/overwrites cookie
+    }
+
+    function failModal(userLevel, userScore) {
+// on run displays modal by adding inner html to elements based on userLevel/userScore info saved in array
+// modal also has button to begin quiz again reminding users of prize
+    }
+
+    function weHaveAWinner(userLevel, userScore) {
+// takes user to winner's area & stores/overwrites cookie
+    }
+
+}
