9f1f5d2caf8d1c60c774a83df0789f154427d9d5
trdownie <trdownie@gmail.com>
2021-04-27 18:40:57 +0000

    winner.js/html: added simply API connection to obtain latest hash in blockchain

--- winner.js
+++ winner.js
@@ -1,9 +1,26 @@
-//-------------------- COOKIES
-//---------- set achieved level (on page load)
-function storeLevel(level) {
-  document.cookie = "userLevel=" + level + ";path=/";
+
+
+document.addEventListener("DOMContentLoaded", async function () {
+
+    // defines the function to fetch the latest hash from the plaintext API
+    let latestHash = await fetch('https://blockchain.info/q/latesthash')
+  
+    // makes sure the hash returned is ok (the server is responding)
+    // code learnt and utilised from https://javascript.info/fetch 
+    if (latestHash.ok) {
+        // if the server responds, hash represents the response
+        var hash = await latestHash.text();
+    }
+    else {
+        // otherwise, show an alert message with the status
+        window.alert("HTTP-Error: " + hash.status);
+    }
+
+    document.getElementById("hash").innerHTML = hash;
+
+    drawArt(hash);
+})
+
+function drawArt(hash){
+    // code to go here that connects with canvas API and draws art
 }
-
-// example below should write cookie {userLevel=6;path=/}
-storeLevel(6)
-
